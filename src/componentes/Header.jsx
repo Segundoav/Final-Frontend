60bb4b44499c9cb39b0b88969c399f681fa7df42
segundoav <segundoav@hotmail.com>
2024-07-16 13:13:37 -0500

    cambio en logo

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.svg'
+
+export const Header = () => {
+  return (
+    <header className="bg-red-500">
+        <img src={logo} alt="" />
+    </header>
+  )
+}
+
